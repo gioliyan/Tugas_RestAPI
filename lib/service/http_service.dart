1d5a547119dede4dea3d9680771ece11c8310148
gioliyan <gioliyan.pratama@gmail.com>
2021-04-11 20:07:08 +0700

    create popular listview

--- lib/service/http_service.dart
+++ lib/service/http_service.dart
@@ -1,18 +1,22 @@
+import 'dart:convert';
 import 'package:http/http.dart' as http;
 import 'dart:io';
+import 'package:tugas_restapi/models/movie.dart';
 
 class HttpService {
   final String apikey = '6f2b71df04dd0a3dcc8bb37b848fbfb6';
   final String baseUrl = 'https://api.themoviedb.org/3/movie/popular?api_key=';
 
-  Future<String> getPopularMovies() async {
+  Future<List> getPopularMovies() async {
     final String uri = baseUrl + apikey;
 
     http.Response result = await http.get(Uri.parse(uri));
     if (result.statusCode == HttpStatus.ok) {
       print("sukses");
-      String response = result.body;
-      return response;
+      final jsonResponse = json.decode(result.body);
+      final moviesMap = jsonResponse['results'];
+      List movies = moviesMap.map((i) => Movie.fromJson(i)).toList();
+      return movies;
     } else {
       print("fail");
       return null;
